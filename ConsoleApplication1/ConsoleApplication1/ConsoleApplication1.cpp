9b10cf1c11c78184fd67e2323d3158e0da8d2714
Vlad2346543 <114310954+Vlad2346543@users.noreply.github.com>
2022-10-23 16:07:12 +0300

    lab1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <conio.h>
+#include <windows.h>
+#include <iostream>
+
+int main()
+{
+	int a;
+	std::cout << "Write a word:";
+	std::cin >> a;
+	std::cout << "You wrote:" << a;
+	_getch();
+}
